916162634c019ed4887f812913763b4ec559dcec
ravenOSS <35054853+ravenOSS@users.noreply.github.com>
2019-05-31 11:36:08 -0600

    Sensor reading strobing sensor Pin4 for reading

--- src/maxPhoton0.cpp
+++ src/maxPhoton0.cpp
@@ -19,10 +19,11 @@
 // This suggests that there are multiple readings being made, stored, and parsed to buf
 
 
+
 void setup();
-void loop ();
-#line 20 "/Users/raventt/MBP_Projects/arduino/maxPhoton0/src/maxPhoton0.ino"
-int maxOn = D0;  // Photon output pin to toggle pin 4 on Maxbotix
+void loop();
+#line 21 "/Users/raventt/MBP_Projects/arduino/maxPhoton0/src/maxPhoton0.ino"
+int maxOn = D0; // Photon output pin to toggle pin 4 on Maxbotix
 
 char buf[5]; // Create buffer to handle RxxxCR
 
@@ -36,23 +37,26 @@ void setup()
   digitalWrite(maxOn, LOW); // Stop continuous reading mode on sensor
 }
 
-unsigned long currentMillis = millis(); // Timing and Serial.print's are for debug 
-
-void loop ()
-{ 
+void loop()
+{
   unsigned long start = micros();
+   Serial1.flush();
+  Serial1.read();            // Ready to read
   digitalWrite(maxOn, HIGH); // Bring HIGH to trigger range reading
-  Serial1.read(); // Only reads one character at a time
-  if (Serial1.available() >=4) // Only iterate over the range data if there is a minimum of Rxxx
+  if (micros() >= start + 20);
+  digitalWrite(maxOn, LOW);     // End range reading
+  if (Serial1.available() >= 4) // Only iterate over the range data if there is a minimum of Rxxx
   {
-    for (int i=0; i<4; i++) {
+    for (int i = 0; i < 5; i++)
+    {
+
       buf[i] = Serial1.read(); // Push data into buffer
     }
   }
-  digitalWrite(maxOn, LOW); // End range reading
-    unsigned long end = micros();
-    unsigned long elapsed = millis() / (60 * 1000); // millis is the time since code was started
-  Serial1.flush();
+
+  unsigned long end = micros();
+  unsigned long elapsed = millis() / (60 * 1000); // millis is the time since code was started
+  // Serial1.flush();
 
   Serial.print("Start: ");
   Serial.println(start);
@@ -62,12 +66,43 @@ void loop ()
 
   Serial.print("Elapsed: ");
   Serial.println(elapsed);
-  
+
   Serial.print("Buf: ");
   Serial.println(buf);
   int data = atoi(buf);
   Serial.print("Parsed: ");
   Serial.println(data);
-  delay(1200); // Repeat the process every X milliseconds
-}
+  delay(1200); // Re
 
+  // void loop ()
+  // {
+  //   unsigned long start = micros();
+  //   digitalWrite(maxOn, HIGH); // Bring HIGH to trigger range reading
+  //   Serial1.read(); // Only reads one character at a time
+  //   if (Serial1.available() >=4) // Only iterate over the range data if there is a minimum of Rxxx
+  //   {
+  //     for (int i=0; i<4; i++) {
+  //       buf[i] = Serial1.read(); // Push data into buffer
+  //     }
+  //   }
+  //   digitalWrite(maxOn, LOW); // End range reading
+  //     unsigned long end = micros();
+  //     unsigned long elapsed = millis() / (60 * 1000); // millis is the time since code was started
+  //   Serial1.flush();
+
+  //   Serial.print("Start: ");
+  //   Serial.println(start);
+
+  //   Serial.print("End: ");
+  //   Serial.println(end);
+
+  //   Serial.print("Elapsed: ");
+  //   Serial.println(elapsed);
+
+  //   Serial.print("Buf: ");
+  //   Serial.println(buf);
+  //   int data = atoi(buf);
+  //   Serial.print("Parsed: ");
+  //   Serial.println(data);
+  //   delay(1200); // Repeat the process every X milliseconds
+}
